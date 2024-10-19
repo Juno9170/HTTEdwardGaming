60d5887feb2e9ce1cdcb4d36e2d3a268368e81c6
Juno9170 <ryanzhuturtle@gmail.com>
2024-10-19 13:49:04 -0400

    Frontend 4

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,13 @@
 import { Central as Layout } from "@/layouts";
 import "./NotFound.style.scss";
-
+import { Link } from "react-router-dom";
 function NotFound() {
   return (
     <Layout title={"Page Not Found"}>
-      <h1>404</h1>
+      <h1>404! Are you sure thats what you're looking for?</h1>
+      <Link to={"/"}>
+        <h2 className=" underline text-blue-300">Click me to go home!</h2>
+      </Link>
     </Layout>
   );
 }
